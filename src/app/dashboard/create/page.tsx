ff35da745c3d2217b4cec5b17d230073972b45f1
Emmanuel odii <emmanuelodii80@gmail.com>
2023-12-19 20:14:55 +0100

    perf: remove unwanted components

--- src/app/dashboard/create/page.tsx
+++ src/app/dashboard/create/page.tsx
@@ -1,4 +1,5 @@
 import { FirstSteps } from "@components/first-steps";
+import { CreateHeader } from "@components/create-header";
 
 interface Props {
   searchParams: {
@@ -8,8 +9,15 @@ interface Props {
 
 export default async function CreatePage({ searchParams: { path } }: Props) {
   return (
-    <main className="w-full">
-      <FirstSteps />
-    </main>
+    <div className="flex flex-col gap-6 lg:gap-12 p-12 overflow-x-hidden">
+      <header className="mt-1">
+        <CreateHeader />
+      </header>
+      <main className="flex justify-center mx-auto items-center max-w-[1350px]">
+        <div className="flex-[0.5]">
+          <FirstSteps />
+        </div>
+      </main>
+    </div>
   );
 }
