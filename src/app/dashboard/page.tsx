82ec7aaca0c3f1c6122a075c1906c1bd45a11910
Emmanuel odii <emmanuelodii80@gmail.com>
2023-12-23 16:07:50 +0100

    fix: redirect to dashboard with session

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,50 +1,49 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
-import { findId, logError } from "@action";
-import supabase from "@lib/supabase";
+import { findId, logError } from '@action'
+import supabase from '@lib/supabase'
 
-import { WaitList } from "@app/types";
-import { LoginComponent } from "@components/login-component";
-import { WaitlistCard } from "@components/waitlist-card";
+import { WaitList } from '@app/types'
+import { LoginComponent } from '@components/login-component'
+import { WaitlistCard } from '@components/waitlist-card'
 
 export async function getUserWaitlists(email: string): Promise<WaitList[]> {
-  const user_id = await findId(email);
+  const user_id = await findId(email)
   const { data, error } = await supabase
-    .from("waitlists")
-    .select("*")
-    .eq("user_id", user_id);
+    .from('waitlists')
+    .select('*')
+    .eq('user_id', user_id)
 
   if (error) {
-    logError(error);
+    logError(error)
   }
 
-  return data!;
+  return data!
 }
 
 export default async function DashboardPage() {
-  const session = await getServerSession();
-  const email = session?.user?.email!;
+  const session = await getServerSession()
+  const email = session?.user?.email!
 
   if (!session || !session.user) {
-    redirect("/sign-in");
+    redirect('/sign-in')
   }
 
-  const userlists = await getUserWaitlists(email);
+  const userlists = await getUserWaitlists(email)
 
   return (
-  
     <>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {userlists?.length > 0 ? (
-            userlists.map((d) => <WaitlistCard key={d.created_at} {...d} />)
-          ) : (
-            <div>You do not have a list yet!</div>
-          )}
-        </div>
-        <div>This is a protected page for {email}</div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {userlists?.length > 0 ? (
+          userlists.map(d => <WaitlistCard key={d.created_at} {...d} />)
+        ) : (
+          <div>You do not have a list yet!</div>
+        )}
+      </div>
+      <div>This is a protected page for {email}</div>
       <LoginComponent />
     </>
-  
-  );
+  )
 }
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import { BetalistHero } from "@components/hero";
+import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
+import { BetalistHero } from '@components/hero'
 
 export default async function LandingPage() {
+  const session = await getServerSession()
+
+  if (session || session!.user) {
+    redirect('/dashboard')
+  }
+
   return (
     <main className="min-h-screen p-24">
       <BetalistHero />
     </main>
-  );
+  )
 }
+
