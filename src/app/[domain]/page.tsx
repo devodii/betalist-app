90b4e187096dcbe786e16a877dd5704e2d27540c
Emmanuel odii <emmanuelodii80@gmail.com>
2023-12-26 12:11:34 +0100

    perf: update domain routes

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -4,18 +4,18 @@ import { WaitListForm } from '@components/waitlist-form'
 
 interface Props {
   params: {
-    page: string
+    domain: string
   }
 }
 
 export default async function WaitlistPage(props: Props) {
-  const url = undoFormatUrl(props.params.page)
+  const url = undoFormatUrl(props.params.domain)
   const { waitlist_table_info: res } = await getWaitlist(url)
 
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-center">
       {res?.id ? (
-        <WaitListForm waitlistInfo={res} product_name={props.params.page} />
+        <WaitListForm waitlistInfo={res} product_name={props.params.domain} />
       ) : (
         <div>Sorry! This page does not exist</div>
       )}
--- src/app/[domain]/thank-you/page.tsx
+++ src/app/[domain]/thank-you/page.tsx
@@ -1,12 +1,12 @@
 interface Props {
   params: {
-    page: string
+    domain: string
   }
 }
 export default async function ThankYouPage(props: Props) {
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-center">
-      <div>{props.params.page} says:</div>
+      <div>{props.params.domain} says:</div>
       Thank you for waiting!
     </main>
   )
