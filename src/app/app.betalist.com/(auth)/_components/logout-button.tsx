58b9299b2eaa297aa04776e9f5ef5f03fae32769
Emmanuel odii <emmanuelodii80@gmail.com>
2024-01-05 09:17:42 +0100

    chore: disable spinner on-click

--- src/app/app.betalist.com/(auth)/_components/logout-button.tsx
+++ src/app/app.betalist.com/(auth)/_components/logout-button.tsx
@@ -1,15 +1,23 @@
 'use client'
 
+import { Spinner } from '@icons'
 import { Button } from '@ui/button'
 import { signOut } from 'next-auth/react'
+import * as React from 'react'
 
 export function LogoutButton() {
+  const [showSpinner, setShowSpinner] = React.useState(false)
   return (
     <Button
-      className="px-4 py-2 rounded-md w-full text-lg"
-      onClick={() => signOut()}
+      className="px-4 py-2 rounded-md w-full"
+      aria-disabled={showSpinner}
+      onClick={() => {
+        setShowSpinner(true)
+        signOut()
+      }}
     >
-      Sign out
+      <span className="text-lg">Sign out</span>
+      {showSpinner && <Spinner size={24} className="ml-1 animate-spin" />}
     </Button>
   )
 }
